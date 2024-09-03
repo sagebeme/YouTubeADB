43b87b927e78866528f09eb8405117285b874399
Sagebeme <93966131+sagebeme@users.noreply.github.com>
2024-09-04 00:06:27 +0300

    Update YouTube.user.js

--- YouTube.user.js
+++ YouTube.user.js
@@ -3,32 +3,32 @@
 // @namespace    https://github.com/sagebeme/YouTubeADB
 // @version      6.03
 // @description  This script removes ads on YouTube, it's lightweight and efficient, capable of smoothly removing interface and video ads, including 6s ads.
-// @author       sagebeme
+// @author       Ngare Macharia
 // @match        *://*.youtube.com/*
 // @exclude      *://accounts.youtube.com/*
 // @exclude      *://www.youtube.com/live_chat_replay*
 // @exclude      *://www.youtube.com/persist_identity*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=YouTube.com
 // @grant        none
-// @license MIT
+// @license      MIT
 // ==/UserScript==
 
 (function() {
-    `use strict`;
+    'use strict';
 
     // Interface advertisement selectors
     const cssSelectorArr = [
-        `#masthead-ad`, // Homepage top banner ad.
-        `ytd-rich-item-renderer.style-scope.ytd-rich-grid-row #content:has(.ytd-display-ad-renderer)`, // Homepage video layout ad.
-        `.video-ads.ytp-ad-module`, // Player bottom ad.
-        `tp-yt-paper-dialog:has(yt-mealbar-promo-renderer)`, // Player page member promotion ad.
-        `ytd-engagement-panel-section-list-renderer[target-id="engagement-panel-ads"]`, // Player page top right recommendation ad.
-        `#related #player-ads`, // Player page comment area right promotion ad.
-        `#related ytd-ad-slot-renderer`, // Player page comment area right video layout ad.
-        `ytd-ad-slot-renderer`, // Search page ad.
-        `yt-mealbar-promo-renderer`, // Player page member recommendation ad.
-        `ad-slot-renderer`, // Mobile player third-party recommendation ad.
-        `ytm-companion-ad-renderer`, // Mobile skippable video ad link
+        '#masthead-ad', // Homepage top banner ad.
+        'ytd-rich-item-renderer.style-scope.ytd-rich-grid-row #content:has(.ytd-display-ad-renderer)', // Homepage video layout ad.
+        '.video-ads.ytp-ad-module', // Player bottom ad.
+        'tp-yt-paper-dialog:has(yt-mealbar-promo-renderer)', // Player page member promotion ad.
+        'ytd-engagement-panel-section-list-renderer[target-id="engagement-panel-ads"]', // Player page top right recommendation ad.
+        '#related #player-ads', // Player page comment area right promotion ad.
+        '#related ytd-ad-slot-renderer', // Player page comment area right video layout ad.
+        'ytd-ad-slot-renderer', // Search page ad.
+        'yt-mealbar-promo-renderer', // Player page member recommendation ad.
+        'ad-slot-renderer', // Mobile player third-party recommendation ad.
+        'ytm-companion-ad-renderer', // Mobile skippable video ad link
     ];
 
     window.dev = false; // Development use
@@ -42,11 +42,11 @@
     function moment(time) {
         // Get year, month, day, hour, minute, and second
         let y = time.getFullYear();
-        let m = (time.getMonth() + 1).toString().padStart(2, `0`);
-        let d = time.getDate().toString().padStart(2, `0`);
-        let h = time.getHours().toString().padStart(2, `0`);
-        let min = time.getMinutes().toString().padStart(2, `0`);
-        let s = time.getSeconds().toString().padStart(2, `0`);
+        let m = (time.getMonth() + 1).toString().padStart(2, '0');
+        let d = time.getDate().toString().padStart(2, '0');
+        let h = time.getHours().toString().padStart(2, '0');
+        let min = time.getMinutes().toString().padStart(2, '0');
+        let s = time.getSeconds().toString().padStart(2, '0');
         return `${y}-${m}-${d} ${h}:${min}:${s}`;
     }
 
@@ -69,9 +69,9 @@
     * @return {undefined}
     */
     function setRunFlag(name) {
-        let style = document.createElement(`style`);
+        let style = document.createElement('style');
         style.id = name;
-        (document.querySelector(`head`) || document.querySelector(`body`)).appendChild(style);
+        (document.querySelector('head') || document.querySelector('body')).appendChild(style);
     }
 
     /**
@@ -105,15 +105,15 @@
     function generateRemoveADHTMLElement(id) {
         // If already set, exit.
         if (checkRunFlag(id)) {
-            log(`Blocked page ad node already generated`);
+            log('Blocked page ad node already generated');
             return false;
         }
 
         // Set remove ad style.
-        let style = document.createElement(`style`);
-        (document.querySelector(`head`) || document.querySelector(`body`)).appendChild(style);
+        let style = document.createElement('style');
+        (document.querySelector('head') || document.querySelector('body')).appendChild(style);
         style.appendChild(document.createTextNode(generateRemoveADCssText(cssSelectorArr)));
-        log(`Successfully generated blocked page ad node`);
+        log('Successfully generated blocked page ad node');
     }
 
     /**
@@ -125,7 +125,7 @@
         cssSelectorArr.forEach((selector, index) => {
             cssSelectorArr[index] = `${selector}{display:none!important}`;
         });
-        return cssSelectorArr.join(` `);
+        return cssSelectorArr.join(' ');
     }
 
     /**
@@ -146,7 +146,7 @@
         });
 
         // Create TouchEvent object
-        let touchStartEvent = new TouchEvent(`touchstart`, {
+        let touchStartEvent = new TouchEvent('touchstart', {
             bubbles: true,
             cancelable: true,
             view: window,
@@ -159,7 +159,7 @@
         this.dispatchEvent(touchStartEvent);
 
         // Create TouchEvent object
-        let touchEndEvent = new TouchEvent(`touchend`, {
+        let touchEndEvent = new TouchEvent('touchend', {
             bubbles: true,
             cancelable: true,
             view: window,
@@ -177,9 +177,9 @@
     * @return {undefined}
     */
     function skipAd(mutationsList, observer) {
-        let video = document.querySelector(`.ad-showing video`) || document.querySelector(`video`);
-        let skipButton = document.querySelector(`.ytp-ad-skip-button`) || document.querySelector(`.ytp-skip-ad-button`) || document.querySelector(`.ytp-ad-skip-button-modern`);
-        let shortAdMsg = document.querySelector(`.video-ads.ytp-ad-module .ytp-ad-player-overlay`) || document.querySelector(`.ytp-ad-button-icon`);
+        let video = document.querySelector('.ad-showing video') || document.querySelector('video');
+        let skipButton = document.querySelector('.ytp-ad-skip-button') || document.querySelector('.ytp-skip-ad-button') || document.querySelector('.ytp-ad-skip-button-modern');
+        let shortAdMsg = document.querySelector('.video-ads.ytp-ad-module .ytp-ad-player-overlay') || document.querySelector('.ytp-ad-button-icon');
 
         if (skipButton) {
             // Mute on mobile has a bug
@@ -188,19 +188,18 @@
             }
             if (video.currentTime > 0.5) {
                 video.currentTime = video.duration;
-                log(`Special account skipped button ad ~~~~~~~~~~~~~`);
+                log('Special account skipped button ad ~~~~~~~~~~~~~');
                 return;
             }
             skipButton.click(); // PC
             nativeTouch.call(skipButton); // Phone
-            log(`Button skipped ad ~~~~~~~~~~~~~`);
+            log('Button skipped ad ~~~~~~~~~~~~~');
         } else if (shortAdMsg) {
             video.currentTime = video.duration;
-            log(`Forcefully ended the ad ~~~~~~~~~~~~~`);
+            log('Forcefully ended the ad ~~~~~~~~~~~~~');
         } else {
-            log(`###### Ad does not exist ######`);
+            log('###### Ad does not exist ######');
         }
-
     }
 
     /**
@@ -210,7 +209,7 @@
     function removePlayerAD(id) {
         // If already running, exit.
         if (checkRunFlag(id)) {
-            log(`Removing ads during playback function is already running`);
+            log('Removing ads during playback function is already running');
             return false;
         }
         let observer;
@@ -219,9 +218,9 @@
         // Start observing
         function startObserve() {
             // Advertisement node listening
-            const targetNode = document.querySelector(`.video-ads.ytp-ad-module`);
+            const targetNode = document.querySelector('.video-ads.ytp-ad-module');
             if (!targetNode) {
-                log(`Finding the target node to be monitored`);
+                log('Finding the target node to be monitored');
                 return false;
             }
             // Listen to and handle ads in videos
@@ -240,22 +239,22 @@
             }
         }, 16);
 
-        log(`Successfully running remove ads during playback function`);
+        log('Successfully running remove ads during playback function');
     }
 
     /**
     * Main function
     */
     function main() {
-        generateRemoveADHTMLElement(`removeADHTMLElement`); // Remove ads from the interface.
-        removePlayerAD(`removePlayerAD`); // Remove ads during playback.
+        generateRemoveADHTMLElement('removeADHTMLElement'); // Remove ads from the interface.
+        removePlayerAD('removePlayerAD'); // Remove ads during playback.
     }
 
-    if (document.readyState === `loading`) {
-        log(`YouTube ad removal script is about to be invoked:`);
-        document.addEventListener(`DOMContentLoaded`, main);
+    if (document.readyState === 'loading') {
+        log('YouTube ad removal script is about to be invoked:');
+        document.addEventListener('DOMContentLoaded', main);
     } else {
-        log(`YouTube ad removal script invoked quickly:`);
+        log('YouTube ad removal script invoked quickly:');
         main();
     }
 
